674fe7902ac3ce407c2fe7461276a3a5f561b90d
Harrison King <authorharrisonking@gmail.com>
2022-03-08 14:35:50 -0700

    basic total products

--- src/app/total-products/total-products.component.ts
+++ src/app/total-products/total-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-total-products',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./total-products.component.scss']
 })
 export class TotalProductsComponent implements OnInit {
+  @Input() total!: number;
 
   constructor() { }
 
